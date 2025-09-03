ce371e154c22e2e584c59ff2b0e251ca4a3500a9
Mofil <mofila@amazon.com>
2025-09-03 11:34:57 +0530

    Create aura.meta.js

---
+++ aura.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Aura
+// @namespace    Paragon_Microsites_NPT_SESU
+// @author       mofila@
+// @description  Automated Utility & Resource Assistant for team Microsites
+// @include      /^https:\/\/paragon-(na|eu|fe)\.amazon\.com\/hz\/lobby$/
+// @require      https://sdk.amazonaws.com/js/aws-sdk-2.1109.0.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/amazon-cognito-identity-js/5.2.1/amazon-cognito-identity.min.js
+// @updateURL    https://raw.githubusercontent.com/Mofi-l/aura-tool/main/aura.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Mofi-l/aura-tool/main/aura.user.js
+// @version      3.0
+// @grant        none
+// ==/UserScript==
